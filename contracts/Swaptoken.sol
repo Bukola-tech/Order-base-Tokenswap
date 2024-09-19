f8ad55d4ea0784aad5fadfe17868195ec3e4db8d
Bukola-tech <152262427+Bukola-tech@users.noreply.github.com>
2024-09-19 18:41:02 +0100

    updated library for events and errors and did the create order function

--- contracts/Swaptoken.sol
+++ contracts/Swaptoken.sol
@@ -1,2 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./libraries/Events.sol";
+import "./libraries/Errors.sol";
+
+contract OrderBasedSwap {
+    uint256 public orderCounter;
+
+    struct Order {
+        uint256 id;
+        address depositor;
+        IERC20 depositToken;
+        uint256 depositAmount;
+        IERC20 desiredToken;
+        uint256 desiredAmount;
+        bool isCompleted;
+        bool isCanceled;
+        address buyer;
+        uint256 timeCreated;
+        uint256 timeCanceled;
+    }
+
+    mapping(uint256 => Order) public orders;
+    mapping(address => uint256[]) public depositorToOrderIds;
+
+    event OrderCreated(uint256 indexed orderId, address indexed depositor, address depositToken, uint256 depositAmount, address desiredToken, uint256 desiredAmount);
+    event OrderCompleted(uint256 indexed orderId, address indexed buyer, address depositToken, uint256 depositAmount, address desiredToken, uint256 desiredAmount);
+    event OrderCanceled(uint256 indexed orderId, address indexed depositor);
+
+    function createOrder(
+        IERC20 _depositToken,
+        uint256 _depositAmount,
+        IERC20 _desiredToken,
+        uint256 _desiredAmount
+    ) external {
+        // Basic checks
+        Errors.sanityCheck(msg.sender);
+        Errors.sanityCheck(address(_depositToken));
+        Errors.sanityCheck(address(_desiredToken));
+        Errors.zeroValueCheck(_depositAmount);
+        Errors.zeroValueCheck(_desiredAmount);
+
+        // Check for sufficient balance and allowance
+        if (_depositAmount > _depositToken.balanceOf(msg.sender)) {
+            revert Errors.InSufficientBalance();
+        }
+
+        if (!_depositToken.transferFrom(msg.sender, address(this), _depositAmount)) {
+            revert Errors.DepositFailed();
+        }
+
+        // Create new order
+        uint256 orderId = ++orderCounter;
+        orders[orderId] = Order({
+            id: orderId,
+            depositor: msg.sender,
+            depositToken: _depositToken,
+            depositAmount: _depositAmount,
+            desiredToken: _desiredToken,
+            desiredAmount: _desiredAmount,
+            isCompleted: false,
+            isCanceled: false,
+            buyer: address(0),
+            timeCreated: block.timestamp,
+            timeCanceled: 0
+        });
+
+        // Track order for depositor
+        depositorToOrderIds[msg.sender].push(orderId);
+
+        // Emit order creation event
+        Events.emitOrderCreated(msg.sender, address(_depositToken), _depositAmount, address(_desiredToken), _desiredAmount, block.timestamp);
+    }
+
+    
+}
